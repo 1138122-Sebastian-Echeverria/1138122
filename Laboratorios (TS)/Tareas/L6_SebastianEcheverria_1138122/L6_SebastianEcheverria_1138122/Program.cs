eff4e9acc5e1a93e3c19c4bc083aafeac4467ad9
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2022-10-07 15:03:44 -0600

    Laboratorios de TS

---
+++ Laboratorios (TS)/Tareas/L6_SebastianEcheverria_1138122/L6_SebastianEcheverria_1138122/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6_SebastianEcheverria_1138122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //verificador de contraseña
+            String contraseniaDos = "";
+            String contrasenia = "";
+            Console.WriteLine("Ingrese una constraseña: ");
+            contraseniaDos = Console.ReadLine();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese la constraseña: ");
+                contrasenia = Console.ReadLine();
+
+            } while (!(contrasenia == contraseniaDos));
+        }
+    }
+}
