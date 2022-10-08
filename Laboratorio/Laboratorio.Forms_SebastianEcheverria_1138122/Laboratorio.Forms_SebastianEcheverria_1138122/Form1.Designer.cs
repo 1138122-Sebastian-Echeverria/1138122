c00034ed1f48c7b8bca72bb565dd597680fa048b
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2022-10-08 11:41:37 -0600

    Lab 8

---
+++ Laboratorio/Laboratorio.Forms_SebastianEcheverria_1138122/Laboratorio.Forms_SebastianEcheverria_1138122/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Laboratorio.Forms_SebastianEcheverria_1138122
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sebastian";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(271, 127);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(241, 20);
+            this.txtN.TabIndex = 1;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(367, 184);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(35, 13);
+            this.Nombre.TabIndex = 2;
+            this.Nombre.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label Nombre;
+    }
+}
+
