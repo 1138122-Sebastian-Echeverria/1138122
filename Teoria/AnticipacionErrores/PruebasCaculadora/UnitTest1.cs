cbdd9d171b40826c98759d2768dbb65884e3ef53
Echeve <secheverriaf@correo.url.edu.gt>
2022-10-25 19:09:36 -0600

    Teoria

---
+++ Teoria/AnticipacionErrores/PruebasCaculadora/UnitTest1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnticipacionErrores;
+using System;
+
+namespace PruebasCaculadora
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculadora miCalculadora = new Calculadora();
+        
+        [TestMethod]
+        public void CasoPruebaSuma1()
+        {
+            int sumando1 = 2;
+            int sumando2 = 3;
+            int total = miCalculadora.suma(sumando1, sumando2);
+            Assert.AreEqual(5, total);
+        }
+
+        [TestMethod]
+        public void CasoPruebaSuma2()
+        {
+            int sumando1 = 0;
+            int sumando2 = 2;
+            int total = miCalculadora.suma(sumando1, sumando2);
+            Assert.AreEqual(2, total);
+        }
+
+
+        [TestMethod]
+        public void RestaPrueba1()
+        {
+            int minuendo = 2;
+            int sustraendo = 1;
+            int diferencia = miCalculadora.resta(minuendo, sustraendo);
+            Assert.AreEqual(1, diferencia);
+        }
+
+        [TestMethod]
+        public void RestaPrueba2()
+        {
+            int minuendo = 3;
+            int sustraendo = 5;
+            int diferencia = miCalculadora.resta(minuendo, sustraendo);
+            Assert.AreEqual(-2, diferencia);
+        }
+
+        [TestMethod]
+        public void RestaPrueba3()
+        {
+            int minuendo = 3;
+            int sustraendo = 0;
+            int diferencia = miCalculadora.resta(minuendo, sustraendo);
+            Assert.AreEqual(3, diferencia);
+        }
+
+        [TestMethod]
+        public void RestaPrueba4()
+        {
+            int minuendo = -3;
+            int sustraendo = 2;
+            int diferencia = miCalculadora.resta(minuendo, sustraendo);
+            Assert.AreEqual(-5, diferencia);
+        }
+
+        [TestMethod]
+        public void RestaPrueba5()
+        {
+            int minuendo = 3;
+            int sustraendo = -4;
+            int diferencia = miCalculadora.resta(minuendo, sustraendo);
+            Assert.AreEqual(7, diferencia);
+        }
+
+        [TestMethod]
+        public void MultiplicacionPrueba1()
+        {
+            int factor1 = 3;
+            int factor2 = 2;
+            int producto = miCalculadora.multiplicacion(factor1, factor2);
+            Assert.AreEqual(6, producto);
+        }
+
+        [TestMethod]
+        public void MultiplicacionPrueba2()
+        {
+            int factor1 = 2;
+            int factor2 = 3;
+            int producto = miCalculadora.multiplicacion(factor1, factor2);
+            Assert.AreEqual(6, producto);
+        }
+
+        [TestMethod]
+        public void MultiplicacionPrueba3()
+        {
+            int factor1 = -3;
+            int factor2 = 2;
+            int producto = miCalculadora.multiplicacion(factor1, factor2);
+            Assert.AreEqual(-6, producto);
+        }
+
+        [TestMethod]
+        public void MultiplicacionPrueba4()
+        {
+            int factor1 = 3;
+            int factor2 = -2;
+            int producto = miCalculadora.multiplicacion(factor1, factor2);
+            Assert.AreEqual(-6, producto);
+        }
+    }
+}
---
+++ Teoria/Operaciones_Con_Sumas/Operaciones_Con_Sumas/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones_Con_Sumas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma 5 + 10 = " + Suma(5, 10));
+            Console.WriteLine("Multiplicacion 5 * 10 = " + Multiplicacion(5, 10));
+            Console.WriteLine("Potencia 5 ^ 3 = " + Potencia(5, 3));
+            Console.WriteLine("Resta 5 - 3 = " + Resta(5, 3));
+            Console.WriteLine("Cociente 5 / 3 = " + Cociente(5, 3));
+            Console.WriteLine("Cociente 9 / 3 = " + Cociente(9, 3));
+            Console.WriteLine("Cociente 3 / 9 = " + Cociente(3, 9));
+            Console.WriteLine("Residuo 5 / 3 = " + Residuo(5, 3));
+            Console.WriteLine("Residuo 9 / 3 = " + Residuo(9, 3));
+            Console.WriteLine("Residuo 3 / 9 = " + Residuo(3, 9));
+
+
+            Console.ReadKey();
+        }
+
+        static int Suma(int sumando1, int sumando2) 
+        {
+            return sumando1 + sumando2;
+        }
+
+        static int Multiplicacion(int factor1, int factor2) 
+        {
+            int acumulador = 0;
+            
+            for (int i = 0; i < factor2; i++) {
+                acumulador = Suma(acumulador, factor1);
+            }
+
+            return acumulador;
+        }
+
+        static int Potencia(int _base, int _exponente) {
+            int acumulador = 1;
+
+            for (int i = 0; i < _exponente; i++)
+            {
+                acumulador = Multiplicacion(acumulador, _base);
+            }
+
+            return acumulador;
+        }
+
+        static int Resta(int minuendo, int sustraendo) {
+            sustraendo = -sustraendo;
+            return Suma(minuendo, sustraendo);
+        }
+
+        static int Cociente(int dividendo, int divisor)
+        {
+            int contadora = 0;
+            int resultado = Resta(dividendo, divisor);
+
+            while (resultado >= 0) {
+                resultado = Resta(resultado, divisor);
+                contadora = contadora + 1; //contadora++;
+            }
+
+            return contadora;
+        }
+
+        static int Residuo(int dividendo,int divisor)
+        {
+            int Res = Cociente(dividendo, divisor);
+            Res = Multiplicacion(divisor, Res);
+            Res = Resta(dividendo, Res);
+            return Res;
+        }
+    }
+}
