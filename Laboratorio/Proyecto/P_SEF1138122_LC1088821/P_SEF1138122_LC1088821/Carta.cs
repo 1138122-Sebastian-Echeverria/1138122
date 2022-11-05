ab27d5226deca548522f00b90951d788772c2cd1
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2022-11-05 11:20:07 -0600

    Add files via upload

---
+++ Laboratorio/Proyecto/P_SEF1138122_LC1088821/P_SEF1138122_LC1088821/Carta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_SEF1138122_LC1088821
+{
+    internal class Carta
+    {
+        public string nombre;
+        public int puntosDeVida;
+        public double daño;
+    }
+}
---
+++ Laboratorio/Proyecto/P_SEF1138122_LC1088821/P_SEF1138122_LC1088821/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_SEF1138122_LC1088821
+{
+    internal class Deck
+    {
+    }
+}
