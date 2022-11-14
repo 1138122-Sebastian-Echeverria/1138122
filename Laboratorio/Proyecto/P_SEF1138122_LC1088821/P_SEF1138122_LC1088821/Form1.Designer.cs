9e6f2f74af1a7402364834b6699518355c0a873c
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2022-11-14 09:32:15 -0600

    Proyecto

--- Laboratorio/Proyecto/P_SEF1138122_LC1088821/P_SEF1138122_LC1088821/Form1.Designer.cs
+++ Laboratorio/Proyecto/P_SEF1138122_LC1088821/P_SEF1138122_LC1088821/Form1.Designer.cs
@@ -33,14 +33,18 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NombreJ1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.EquipoJ1 = new System.Windows.Forms.ComboBox();
+            this.ApellidoJ1 = new System.Windows.Forms.TextBox();
+            this.ApellidoJ2 = new System.Windows.Forms.TextBox();
+            this.EquipoJ2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Apellido = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.NombreJ2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -80,85 +84,127 @@
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
-            // textBox1
+            // NombreJ1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.NombreJ1.Location = new System.Drawing.Point(79, 38);
+            this.NombreJ1.Name = "NombreJ1";
+            this.NombreJ1.Size = new System.Drawing.Size(100, 20);
+            this.NombreJ1.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(111, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nombre";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(111, 68);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.label2.Text = "Apellido";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(444, 22);
+            this.label3.Location = new System.Drawing.Point(111, 117);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
+            this.label3.Text = "Equipo";
             // 
-            // textBox3
+            // EquipoJ1
             // 
-            this.textBox3.Location = new System.Drawing.Point(412, 38);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 8;
+            this.EquipoJ1.FormattingEnabled = true;
+            this.EquipoJ1.Items.AddRange(new object[] {
+            "Team Queso",
+            "Golden Wind"});
+            this.EquipoJ1.Location = new System.Drawing.Point(69, 133);
+            this.EquipoJ1.Name = "EquipoJ1";
+            this.EquipoJ1.Size = new System.Drawing.Size(121, 21);
+            this.EquipoJ1.TabIndex = 12;
+            // 
+            // ApellidoJ1
+            // 
+            this.ApellidoJ1.Location = new System.Drawing.Point(79, 84);
+            this.ApellidoJ1.Name = "ApellidoJ1";
+            this.ApellidoJ1.Size = new System.Drawing.Size(100, 20);
+            this.ApellidoJ1.TabIndex = 14;
+            // 
+            // ApellidoJ2
+            // 
+            this.ApellidoJ2.Location = new System.Drawing.Point(405, 84);
+            this.ApellidoJ2.Name = "ApellidoJ2";
+            this.ApellidoJ2.Size = new System.Drawing.Size(100, 20);
+            this.ApellidoJ2.TabIndex = 20;
+            // 
+            // EquipoJ2
+            // 
+            this.EquipoJ2.FormattingEnabled = true;
+            this.EquipoJ2.Items.AddRange(new object[] {
+            "Team Queso",
+            "Golden Wind"});
+            this.EquipoJ2.Location = new System.Drawing.Point(395, 133);
+            this.EquipoJ2.Name = "EquipoJ2";
+            this.EquipoJ2.Size = new System.Drawing.Size(121, 21);
+            this.EquipoJ2.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(437, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Equipo";
+            // 
+            // Apellido
+            // 
+            this.Apellido.AutoSize = true;
+            this.Apellido.Location = new System.Drawing.Point(437, 68);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(35, 13);
+            this.Apellido.TabIndex = 17;
+            this.Apellido.Text = "label4";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(444, 68);
+            this.label4.Location = new System.Drawing.Point(437, 22);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Nombre";
             // 
-            // comboBox1
+            // NombreJ2
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(70, 84);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 12;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(400, 84);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 13;
+            this.NombreJ2.Location = new System.Drawing.Point(405, 38);
+            this.NombreJ2.Name = "NombreJ2";
+            this.NombreJ2.Size = new System.Drawing.Size(100, 20);
+            this.NombreJ2.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(579, 367);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ApellidoJ2);
+            this.Controls.Add(this.EquipoJ2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Apellido);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.NombreJ2);
+            this.Controls.Add(this.ApellidoJ1);
+            this.Controls.Add(this.EquipoJ1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NombreJ1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -179,14 +225,18 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox NombreJ1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox EquipoJ1;
+        private System.Windows.Forms.TextBox ApellidoJ1;
+        private System.Windows.Forms.TextBox ApellidoJ2;
+        private System.Windows.Forms.ComboBox EquipoJ2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Apellido;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox NombreJ2;
     }
 }
 
