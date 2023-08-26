fd2397a15095060540e5cf975bcef35ac2360033
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2023-08-26 08:50:43 -0600

    Lab_2

---
+++ Progra Avanzada/Lab_1/Ej_2/Ej_2/Ej_2.cpp
@@ -0,0 +1,26 @@
+// Ej_2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int ProductoPunto(float *array1, float *array2, int componentes) 
+{
+	int x;
+	int y;
+	int z;
+	int a;
+	int b;
+	int c;
+
+	for (size_t i = 0; i < componentes; i++)
+	{
+
+	}
+}
+
+int main()
+{
+    
+}
+
