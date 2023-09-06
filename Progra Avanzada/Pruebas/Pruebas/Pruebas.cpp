b89454a00e3087bbd355539d5fd7e4b6595ff325
Echeve <secheverriaf@correo.url.edu.gt>
2023-09-06 00:18:48 -0600

    Trabajo

--- Progra Avanzada/Pruebas/Pruebas/Pruebas.cpp
+++ Progra Avanzada/Pruebas/Pruebas/Pruebas.cpp
@@ -1,20 +1,87 @@
-// Pruebas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
-using namespace std;
-int main()
-{
-    int loteria[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    std::cout << *(loteria + 9) << std::endl;
+#include <string>
+
+// Función para determinar si un año es bisiesto
+bool esBisiesto(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+// Función para imprimir un mes dado el número de días en el mes y el día de inicio
+void imprimirMes(int mes, int diasEnMes, int diaInicio) {
+    // Nombres de los días de la semana
+    std::string diasSemana[7] = { "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom" };
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+    // Nombres de los meses
+    std::string meses[12] = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+
+    // Imprimir el nombre del mes
+    std::cout << meses[mes - 1] << std::endl;
+
+    // Imprimir los nombres de los días de la semana
+    for (int i = 0; i < 7; i++) {
+        std::cout << diasSemana[i] << "\t";
+    }
+    std::cout << std::endl;
+
+    // Llenar el calendario con los números de los días
+    int dia = 1;
+    for (int semana = 0; semana < 5; semana++) {
+        for (int diaSemana = 1; diaSemana <= 7; diaSemana++) {
+            if (semana == 0 && diaSemana < diaInicio) {
+                std::cout << " \t";
+            }
+            else if (dia <= diasEnMes) {
+                std::cout << dia << "\t";
+                dia++;
+            }
+            else {
+                std::cout << " \t";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int diaInicio; // Día de la semana en que inicia el año (1=Lunes, 2=Martes, ..., 7=Domingo)
+    int year;      // Año para el que se generará el calendario
+    int opcion;    // Opción para indicar si el año es bisiesto o no
+
+    // Solicitar las condiciones al usuario
+    std::cout << "Ingrese el día de la semana en que inicia el año (1=Lunes, 2=Martes, ..., 7=Domingo): ";
+    std::cin >> diaInicio;
+
+    std::cout << "¿El año es bisiesto? (0=No, 1=Sí): ";
+    std::cin >> opcion;
+
+    std::cout << "Ingrese el año: ";
+    std::cin >> year;
+
+    // Validar el día de inicio (debe estar en el rango 1-7)
+    if (diaInicio < 1 || diaInicio > 7) {
+        std::cerr << "Error: El día de la semana de inicio debe estar en el rango 1-7." << std::endl;
+        return 1;
+    }
+
+    // Ajustar el día de inicio para que sea 0 si es lunes (en lugar de 1)
+    if (diaInicio == 1) {
+        diaInicio = 0;
+    }
+    else {
+        diaInicio--;
+    }
+
+    // Matriz con el número de días en cada mes (febrero se actualiza si es bisiesto)
+    int diasEnMes[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    if (opcion == 1 && esBisiesto(year)) {
+        diasEnMes[1] = 29; // Febrero tiene 29 días en años bisiestos
+    }
+
+    // Generar e imprimir el calendario anual
+    for (int mes = 1; mes <= 12; mes++) {
+        imprimirMes(mes, diasEnMes[mes - 1], diaInicio);
+        diaInicio = (diaInicio + diasEnMes[mes - 1]) % 7;
+    }
+
+    return 0;
+}
