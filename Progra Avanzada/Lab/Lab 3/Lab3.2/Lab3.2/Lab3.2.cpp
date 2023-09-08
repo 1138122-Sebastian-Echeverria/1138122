9d547967e78a71ad461d0af1043b96ddb28daf9c
Echeve <secheverriaf@correo.url.edu.gt>
2023-09-07 20:48:58 -0600

    Semana 3 lab 1

--- Progra Avanzada/Lab/Lab 3/Lab3.2/Lab3.2/Lab3.2.cpp
+++ Progra Avanzada/Lab/Lab 3/Lab3.2/Lab3.2/Lab3.2.cpp
@@ -1,30 +1,25 @@
-#include <iostream>
 using namespace std;
+#include <iostream>
 #include <string>
 
 int main() {
-    string meses[12] = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
-    string dSemana[7] = { "Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom" };
-    int Inicia;
-    int Ano;      // Año para el que se generará el calendario
-    int opcion;    // Opción para indicar si el año es bisiesto o no
-    int DiasV = 0;
+    int Inicia; // Día de la semana en que inicia el año (1=Lunes, 2=Martes, ..., 7=Domingo)
+    int year;      // Año para el que se generará el calendario
+    int Op;    // Opción para indicar si el año es bisiesto o no
 
     // Solicitar las condiciones al usuario
-    cout << "Ingrese el dia de la semana en que inicia el ano (1=Lunes, 2=Martes, ..., 7=Domingo): ";
+    cout << "Ingrese el día de la semana en que inicia el ano (1=Lunes, 2=Martes, ..., 7=Domingo): ";
     cin >> Inicia;
 
-    cout << "El ano es bisiesto? (0=No, 1=Si): ";
-    cin >> opcion;
+    cout << "¿El año es bisiesto? (0=No, 1=Si): ";
+    cin >> Op;
 
     cout << "Ingrese el ano: ";
-    cin >> Ano;
-
-    cout << endl;
+    cin >> year;
 
     // Validar el día de inicio (debe estar en el rango 1-7)
     if (Inicia < 1 || Inicia > 7) {
-        cerr << "Error: El día de la semana de inicio debe estar en el rango 1-7." << endl;
+        cerr << "Error: El dia de la semana de inicio debe estar en el rango 1-7." << endl;
         return 1;
     }
 
@@ -32,73 +27,53 @@ int main() {
     if (Inicia == 1) {
         Inicia = 0;
     }
-
-    // Matriz con el número de días en cada mes (febrero se actualiza si es bisiesto)
-    int diasEnMes[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (opcion == 1) {
-        diasEnMes[1] = 29; // Febrero tiene 29 días en años bisiestos
+    else {
+        Inicia--;
     }
 
-    cout << "Calendario: " << Ano << endl;
+    // Matriz con el número de días en cada mes (febrero se actualiza si es bisiesto)
+    int diasmes[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    if (Op == 1) {
+        diasmes[1] = 29; // Febrero tiene 29 días en años bisiestos
+    }
+
+    // Nombres de los días de la semana
+    string diasS[7] = { "Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom" };
+
+    // Nombres de los meses
+    string meses[12] = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
 
     // Generar e imprimir el calendario anual
-    for (int mes = 1; mes < 12; mes++) {
-        
-        cout << meses[mes-1] << endl;
+    for (int mes = 0; mes < 12; mes++) {
+        // Imprimir el nombre del mes
+        cout << meses[mes] << endl;
 
+        // Imprimir los nombres de los días de la semana
         for (int i = 0; i < 7; i++) {
-            cout << dSemana[i] << "\t";
+            cout << diasS[i] << "\t";
         }
         cout << endl;
-        
 
+        // Llenar el calendario con los números de los días
         int dia = 1;
-
-        if (mes > 1)
-        {
-            if (Inicia >= DiasV)
-            {
-                Inicia = (Inicia + DiasV + 1);
-
-                if (Inicia > 7)
-                {
-                    Inicia = Inicia - 7;
-                }
-            }
-            else
-            {
-                Inicia = (Inicia + (DiasV - Inicia) + 1);
-                if (Inicia > 7)
-                {
-                    Inicia = Inicia - 7;
-                }
-            }
-            
-        }
-           
-       
-
-        for (int semana = 0; semana < 5; semana++)
-        {
-            
-            
-            for (int diaS = 1; diaS <= 7; diaS++) {
-                if (semana == 0 && diaS < Inicia) {
+        for (int semana = 0; semana < 5; semana++) {
+            for (int diaSemana = 0; diaSemana < 7; diaSemana++) {
+                if (semana == 0 && diaSemana < Inicia) {
                     cout << " \t";
                 }
-                else if (dia <= diasEnMes[mes - 1]) {
+                else if (dia <= diasmes[mes]) {
                     cout << dia << "\t";
                     dia++;
                 }
                 else {
                     cout << " \t";
-                    DiasV++;
-
                 }
             }
             cout << endl;
-            cout << endl; 
         }
+
+        Inicia = (Inicia + diasmes[mes]) % 7;
     }
+
     return 0;
 }
