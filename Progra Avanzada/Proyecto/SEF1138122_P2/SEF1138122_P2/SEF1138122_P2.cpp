309d22f4d6abb0c5d430c3c1bbb17428dccbd591
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2023-11-06 23:13:45 -0600

    Proyecto 2

---
+++ Progra Avanzada/Proyecto/SEF1138122_P2/SEF1138122_P2/SEF1138122_P2.cpp
@@ -0,0 +1,9 @@
+// SEF1138122_P2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
