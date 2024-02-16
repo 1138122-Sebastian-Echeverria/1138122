95943f0ddeff2d44a758ac35be25b666e628d3cc
Echeve <111195658+1138122-Sebastian-Echeverria@users.noreply.github.com>
2024-02-15 18:47:03 -0600

    Nuevo año nuevo yo

---
+++ BDD/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <jsoncpp/json/json.h>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
--- Progra Avanzada/Teoria/S7_T1/Implementacion/Implementacion/Nodo.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Joseph Abraham Soto on 3/10/23.
-//
-
-#include "Nodo.h"
